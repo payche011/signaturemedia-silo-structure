faddc31bde8349d5afc22c10245d06b6e2766d7e
Pavle Marinkovic <payche255@gmail.com>
2025-09-21 10:53:39 +0200

    v2.3.2a

--- signaturemedia-silo-structure.php
+++ signaturemedia-silo-structure.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Signature Media Silo Structure
 Description: Enhanced silo content structure with toggleable features and additional content types.
-Version: 2.3.2
+Version: 2.3.2a
 Author: signaturemedia
 Author URI: https://signaturemedia.com/
 Text Domain: signaturemedia-silo-structure
